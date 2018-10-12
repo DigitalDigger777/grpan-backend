6a45a76e57795c54092da89e583b262ac6d7f623
korman <korman.yuri@gmail.com>
2018-10-13 02:21:06 +0300

    fixed bugs

--- app/Http/Controllers/Rest/GameCategoryController.php
+++ app/Http/Controllers/Rest/GameCategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Rest;
 
+use App\GameCategory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +15,8 @@ class GameCategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = GameCategory::with('games')->get();
+        return response()->json($categories);
     }
 
     /**
