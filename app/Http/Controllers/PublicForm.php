001dbfcf2151411d127852c1fc1ef9ea3611884e
korman <korman.yuri@gmail.com>
2018-10-14 03:46:50 +0300

    Fixed bugs

--- app/Http/Controllers/PublicForm.php
+++ app/Http/Controllers/PublicForm.php
@@ -29,15 +29,15 @@ class PublicForm extends Controller
         ];
 
 
-        Mail::send('emails.public', $data, function ($message) {
-            $setting = Setting::all();
-            $message->from('no-reply@greenpanda.ceant.net', 'New publication request');
-            $message->to('korman.yuri@gmail.com');
-            if (count($setting) > 0) {
-
-            }
-
-        });
+//        Mail::send('emails.public', $data, function ($message) {
+//            $setting = Setting::all();
+//            $message->from('no-reply@greenpanda.ceant.net', 'New publication request');
+//            $message->to('korman.yuri@gmail.com');
+//            if (count($setting) > 0) {
+//
+//            }
+//
+//        });
 
         return response()->json(['message' => 'Request completed']);
     }
