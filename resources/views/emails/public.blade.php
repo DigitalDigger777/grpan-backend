1799bac4874230532ef33b9b03f5c21d7eb11712
korman <korman.yuri@gmail.com>
2018-10-19 03:28:03 +0300

    Fixed bugs

--- resources/views/emails/public.blade.php
+++ resources/views/emails/public.blade.php
@@ -2,11 +2,11 @@
 <head></head>
 <body style="background: black; color: white">
 <h1>New public request</h1>
-<p>{{$name}}</p>
-<p>{{$company}}</p>
-<p>{{$game_url}}</p>
-<p>{{$email}}</p>
-<p>{{$skype}}</p>
-<p>{{$message}}</p>
+{{--<p>{{$name}}</p>--}}
+{{--<p>{{$company}}</p>--}}
+{{--<p>{{$game_url}}</p>--}}
+{{--<p>{{$email}}</p>--}}
+{{--<p>{{$skype}}</p>--}}
+{{--<p>{{$message}}</p>--}}
 </body>
 </html>
