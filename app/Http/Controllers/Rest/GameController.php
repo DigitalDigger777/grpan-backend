228c39338e0b2b5cbecc36afdf02c948238d98e2
korman <korman.yuri@gmail.com>
2018-10-24 18:16:30 +0300

    changed email templates

--- app/Http/Controllers/Rest/GameController.php
+++ app/Http/Controllers/Rest/GameController.php
@@ -19,8 +19,7 @@ class GameController extends Controller
         $games = Game::with([
             'category' => function($query){
                 $query->orderBy('id', 'ASC');
-            },
-            'game'
+            }
         ])->where('locale','=', $locale)->get();
         return response()->json($games);
     }
