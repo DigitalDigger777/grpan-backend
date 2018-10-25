e69cf08a9513a1d0e77186cdaab21cbe2849e560
Xavier Menu <xavier@greenpandagames.com>
2018-10-25 17:40:25 +0200

    fix database settings and migrations JSON type

--- database/migrations/2018_10_11_200819_create_settings_table.php
+++ database/migrations/2018_10_11_200819_create_settings_table.php
@@ -16,7 +16,7 @@ class CreateSettingsTable extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->increments('id');
             $table->string('locale');
-            $table->json('data');
+            $table->text('data');
             $table->timestamps();
         });
     }
